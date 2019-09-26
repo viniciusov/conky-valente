84e41f72cc7ec7fe24b8b4398329e8c5c2b88ac7
Vinícius Orsi Valente <viniciusov@hotmail.com>
2019-09-26 12:08:24 -0300

    Installer updated

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ echo "Installing..."
 cp application/conky-valente.desktop $HOME/.config/autostart
 sudo apt install conky-all lm-sensors w3m
 sudo cp -r fonts/truetype/conky_valente /usr/share/fonts/truetype/
-
+sudo cp -r usr/share/conky_valente usr/share/
 echo "Done!"
 
