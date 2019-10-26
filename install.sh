792d059dacd23e996e9e04a3a4431a875a5b4805
Vinícius Orsi Valente <viniciusov@hotmail.com>
2019-10-26 15:14:13 -0300

    Weather removed

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "Installing..."
 
 cp application/conky-valente.desktop $HOME/.config/autostart
-sudo apt install conky-all lm-sensors w3m
+sudo apt install conky-all lm-sensors
 sudo cp -r fonts/truetype/conky_valente /usr/share/fonts/truetype/
 sudo cp -r usr/share/conky_valente /usr/share/
 echo "Done!"
